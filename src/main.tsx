7ffbc34bcad1ab6cd9155d706207d923b7c04319
Mag <166319982+magali-la@users.noreply.github.com>
2025-12-15 09:56:55 -0500

    feat(material UI): add material UI as a dependency, import font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// fonts
+import '@fontsource/iosevka-etoile/300.css';
+import '@fontsource/iosevka-etoile/400.css';
+import '@fontsource/iosevka-etoile/500.css';
+import '@fontsource/iosevka-etoile/600.css';
+import '@fontsource/iosevka-etoile/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
